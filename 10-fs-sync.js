b606cdfcaf2b64d4e80a9b28fb39ec383a9cb217
Juan <juaniwk3@gmail.com>
2021-10-08 13:12:17 -0300

    async vs sync

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,10 +1,19 @@
-import { readFileSync, writeFileSync } from 'fs';
-
+const { readFileSync, writeFileSync } = require('fs');
+console.log('start');
 const first = readFileSync('./content/first.txt');
 const second = readFileSync('./content/second.txt');
 
 writeFileSync(
   './content/result-sync.txt',
   `Here is the result: ${first}, ${second}`,
-  { flag: 'a' }
+  { flag: 'a' }, (err, result) => {
+    if (err) {
+      console.log(err);
+      return
+    }
+    console.log('result');
+  }
 )
+console.log('done with this task');
+console.log('starting the next one');
+
